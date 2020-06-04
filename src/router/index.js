b8e6309e94d9c9bd81b8ca24e8d51845a51a0ce0
Ten <782555894@qq.com>
2020-06-04 09:39:35 +0800

    为路由对象，添加beforeEach导航守守卫

--- src/router/index.js
+++ src/router/index.js
@@ -18,4 +18,15 @@ const router = new VueRouter({
   routes
 })
 
+// 为路由对象，添加 beforeEach 导航守卫
+router.beforeEach((to, from, next) => {
+  // 如果用户访问的登录页，直接放行
+  if (to.path === '/login') return next()
+  // 从 sessionStorage 中获取到 保存的 token 值
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token，强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 export default router
